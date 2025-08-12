e0eee35569fe334e8065c410ec2a36c4b7d0c929
Michanoku <michael.werker@gmail.com>
2025-08-12 23:44:46 +0000

    Add winning condition test

--- script.js
+++ script.js
@@ -10,23 +10,22 @@ const gameboard = (function () {
     // Check for winning condition
     // if [0][0] is equal to [1][1] and [2][2] or [0][2] is equal to [1][1] and [2][0]
     if ((boardArray[0][0] === boardArray[1][1] && boardArray[1][1] === boardArray[2][2]) || (boardArray[0][2] === boardArray[1][1] && boardArray[1][1] === boardArray[2][0])) {
-
+      console.log("WE HAVE A WINNER!")
     } else {
 
     }
     for (let i = 0; i < 3; i++) {
       // if [0][i] is equal to [1][i] and [2][i] or if [i][0] is equal to [i][1] and [i][2]
       if ((boardArray[0][i] === boardArray[1][i] && boardArray[1][i] === boardArray[2][i]) || (boardArray[i][0] === boardArray[i][1] && boardArray[i][1] === boardArray[i][2])) {
-
+        console.log("WE HAVE A WINNER!")
       } 
     }
-
-
   };
 
   const playCell = function(x, y, player) {
     boardArray[x][y] = player.symbol;
     console.log(boardArray)
+    checkProgress();
   }
   return{ playCell };
 })();
