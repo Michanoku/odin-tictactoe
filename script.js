c7eeecf45cbcbad0139a16d1e9053e903c54260e
Michanoku <michael.werker@gmail.com>
2025-08-13 00:06:39 +0000

    Change winning logic

--- script.js
+++ script.js
@@ -7,18 +7,28 @@ const gameboard = (function () {
   ];
 
   function checkProgress() {
+    function checkLine(coordA, coordB, coordC) {
+      if (coordA !== null && (coordA === coordB && coordB === coordC)) {
+        return coordA;
+      } else {
+        return null;
+      }
+    }
     // Check for winning condition
     // if [0][0] is equal to [1][1] and [2][2] or [0][2] is equal to [1][1] and [2][0]
-    if ((boardArray[0][0] === boardArray[1][1] && boardArray[1][1] === boardArray[2][2]) || (boardArray[0][2] === boardArray[1][1] && boardArray[1][1] === boardArray[2][0])) {
-      console.log("WE HAVE A WINNER!")
-    } else {
-
+    // if [0][i] is equal to [1][i] and [2][i] or if [i][0] is equal to [i][1] and [i][2]
+    const lines = [[0, 0, 1, 1, 2, 2], [0, 2, 1, 1, 2, 0]];
+    for (let i = 0; i < 3; i ++) {
+      lines.push([0, i, 1, i, 2, i]);
+      lines.push([i, 0, i, 1, i, 2]);
     }
-    for (let i = 0; i < 3; i++) {
-      // if [0][i] is equal to [1][i] and [2][i] or if [i][0] is equal to [i][1] and [i][2]
-      if ((boardArray[0][i] === boardArray[1][i] && boardArray[1][i] === boardArray[2][i]) || (boardArray[i][0] === boardArray[i][1] && boardArray[i][1] === boardArray[i][2])) {
+    let result;
+    for (const line of lines) {
+      result = checkLine(boardArray[line[0]][line[1]], boardArray[line[2]][line[3]], boardArray[line[4]][line[5]]);
+      if (result !== null) {
         console.log("WE HAVE A WINNER!")
-      } 
+        return result;
+      }
     }
   };
 
